65fba194536518b1c78488303082c4987e4b31e6
Jean-Baptiste Queru <jbqueru@gmail.com>
2025-02-08 10:11:31 +0200

    Begin the work to move the intro logo out

--- intro.s
+++ intro.s
@@ -41,6 +41,10 @@ Intro:
   movem.l d0-d7, GFX_PALETTE.w
 
 IntroLoop:
+
+  cmpi.w #24, vbl_count.l
+  bgt.w IntroInDone.l
+
   lea.l IntroLogo.l, a0
   movea.l gfx_fb_back, a1
   lea.l 16 + 30 * 160(a1), a1
@@ -86,6 +90,42 @@ IntroLoop:
   addq.w #1, d7
 
   dbra.w d6, .CopySlice.l
+
+IntroInDone:
+
+  cmp.w #INTRO_DURATION - 50, vbl_count.l
+  blt.s IntroOutNotYet.l
+
+  lea.l IntroLogo.l, a0
+  movea.l gfx_fb_back, a1
+  lea.l 16 + 30 * 160(a1), a1
+
+  move.w #INTRO_DURATION, d7
+  sub.w vbl_count.l, d7
+
+  moveq.l #15, d6
+.CopySlice:
+
+
+
+  moveq.l #8, d5
+  bra.s .CopyLineLoop.l
+.CopyLine:
+  .rept 32
+  move.l (a0)+, (a1)+
+  .endr
+  lea.l 32(a1), a1
+.CopyLineLoop:
+  dbra.w d5, .CopyLine.l
+
+
+
+
+  dbra.w d6, .CopySlice.l
+
+IntroOutNotYet:
+
+
 
   move.l gfx_fb_front.l, d0
   move.l gfx_fb_back.l, gfx_fb_front.l
